8b2c44e83997588a7487cd4aeeb09c98b54a0f90
Kevin Fung <kfung@tinycranes.com>
2015-06-18 11:55:55 -0400

    Add Basic Window Example

---
+++ Glitter/Headers/glitter.hpp
@@ -0,0 +1,15 @@
+// Preprocessor Directives
+#define STB_IMAGE_IMPLEMENTATION
+
+// System Headers
+#include <assimp/importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <stb_image.h>
+
+// Define Some Constants
+const int mWidth = 1280;
+const int mHeight = 800;
---
+++ Glitter/Sources/main.cpp
@@ -0,0 +1,42 @@
+// Local Headers
+#include "glitter.hpp"
+
+// System Headers
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char * argv[]) {
+
+    // Load GLFW and Create a Window
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
+
+    // Create Context and Load OpenGL Functions
+    glfwMakeContextCurrent(mWindow);
+    gladLoadGL();
+    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+
+    // Rendering Loop
+    while (glfwWindowShouldClose(mWindow) == false) {
+        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(mWindow, true);
+
+        // Background Fill Color
+        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Flip Buffers and Draw
+        glfwSwapBuffers(mWindow);
+        glfwPollEvents();
+    }   glfwTerminate();
+    return EXIT_SUCCESS;
+}
