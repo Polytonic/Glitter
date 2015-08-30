e84791920ca3edb75c8a1c7edd350eae700b1863
Kevin Fung <kfung@tinycranes.com>
2015-08-30 09:39:45 -0700

    Use Base OpenGL 4.0 (See #12)

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char * argv[]) {
     // Load GLFW and Create a Window
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
