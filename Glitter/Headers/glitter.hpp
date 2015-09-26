747a4bc17274ca4d50415626e6f92809b0558b37
Kevin Fung <kfung@tinycranes.com>
2015-09-25 19:09:24 -0700

    Fix Indentation

--- Glitter/Headers/glitter.hpp
+++ Glitter/Headers/glitter.hpp
@@ -11,9 +11,9 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-//  Reference: https://github.com/nothings/stb/blob/master/stb_image.h#L4
-//  To use stb_image, add this in *one* C++ source file.
-//      #define STB_IMAGE_IMPLEMENTATION
+// Reference: https://github.com/nothings/stb/blob/master/stb_image.h#L4
+// To use stb_image, add this in *one* C++ source file.
+//     #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
 // Define Some Constants
