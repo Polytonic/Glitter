ab9ed80bef1d65c1dd2ce2585a43b12ee2f8615c
Feynman Liang <feynman.liang@gmail.com>
2016-05-24 17:19:38 +0100

    Uses key_callback

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -9,6 +9,12 @@
 #include <cstdio>
 #include <cstdlib>
 
+// Callback for handling key presses
+void key_callback(GLFWwindow* mWindow, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(mWindow, true);
+}
+
 int main(int argc, char * argv[]) {
 
     // Load GLFW and Create a Window
@@ -26,16 +32,14 @@ int main(int argc, char * argv[]) {
         return EXIT_FAILURE;
     }
 
-    // Create Context and Load OpenGL Functions
+    // Create Context, Load OpenGL Functions, and register key_callback
     glfwMakeContextCurrent(mWindow);
     gladLoadGL();
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+    glfwSetKeyCallback(mWindow, key_callback);
 
     // Rendering Loop
     while (glfwWindowShouldClose(mWindow) == false) {
-        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(mWindow, true);
-
         // Background Fill Color
         glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
