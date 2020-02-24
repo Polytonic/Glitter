396f07ea4f695bab21e79e1f8bbf58ecd4f64396
John Alexander <jalexander.qed@gmail.com>
2020-02-23 19:31:10 -0800

    Fix unix run script to return exit code of compilation/run commands

--- unirun.sh
+++ unirun.sh
@@ -4,6 +4,8 @@ STARTING_DIR=$(pwd)
 cp Glitter/Shaders/* Build/Glitter/;
 cd Build
 make -j 8 &&
-    cd Glitter &&
-    time ./Glitter "$@"
+	     cd Glitter &&
+	     time ./Glitter "$@"
+RESULT=$?
 cd $STARTING_DIR
+exit $RESULT
