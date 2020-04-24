05b96f1150cfee98ffe10a691fe1481ab218b5d5
John Alexander <jalexander.qed@gmail.com>
2020-04-23 20:42:55 -0700

    Compile and run release version on Windows.

--- wrun.bat
+++ wrun.bat
@@ -3,7 +3,7 @@
 SET STARTING_DIR=%cd%
 
 cd Build && ^
-msbuild Glitter.sln && ^
-cd Glitter\Debug && ^
+msbuild Glitter.sln /p:Configuration=Release && ^
+cd Glitter\Release && ^
 Glitter.exe %*
 cd %STARTING_DIR%
