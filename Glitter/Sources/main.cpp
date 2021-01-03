5758ab9c5c56c22fed56392ab9b97ae182e3c789
pasan nissanka <pasannissanka@hotmail.com>
2021-01-03 20:47:58 +0530

    Added Imgui Library

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -1,14 +1,18 @@
 /* https://learnopengl.com/ 
 
 */
-
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
-
 // Local Headers
 #include "glitter.hpp"
 #include "Shader.hpp"
 #include "Camera.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+// Imgui Headers
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 
 // System Headers
 #include <glad/glad.h>
@@ -25,6 +29,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void loadTexture(unsigned int& texture, std::string source, bool isFlipVerticle, GLint internalFormat, GLint format);
 
 // -------------------------------------------------------------------------------------------------------------
 // GLOBAL VARIABLES
@@ -33,17 +38,20 @@ Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float lastX     =   mWidth / 2.0f;
 float lastY     =   mHeight / 2.0f;
 bool firstMouse =   true;
-
 // timing
 float deltaTime =   0.0f;	// Time between current frame and last frame
 float lastFrame =   0.0f; // Time of last frame
+// imgui
+bool show_demo_window = true;
+bool show_another_window = false;
+ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 // -------------------------------------------------------------------------------------------------------------
 int main(int argc, char* argv[]) {
-
     // ---------------------------------------------------------------------------------------------------------
     // Load GLFW and Create a Window
     glfwInit();
+    const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -56,12 +64,25 @@ int main(int argc, char* argv[]) {
         std::cout << "Failed to Create OpenGL Context" << std::endl;
         return EXIT_FAILURE;
     }
-
+    
     // Create Context and Load OpenGL Functions
     glfwMakeContextCurrent(mWindow);
     glfwSetFramebufferSizeCallback(mWindow, framebuffer_size_callback);
-    gladLoadGL();
+    
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     std::cout << "OpenGL" << glGetString(GL_VERSION) << std::endl;
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark(); //ImGui::StyleColorsClassic();
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(mWindow, true); ImGui_ImplOpenGL3_Init(glsl_version);
 
     glfwSetCursorPosCallback(mWindow, mouse_callback);
     glfwSetScrollCallback(mWindow, scroll_callback);
@@ -72,60 +93,15 @@ int main(int argc, char* argv[]) {
 
     // ---------------------------------------------------------------------------------------------------------
     // SHADER INIT
-
     Shader shader("vertexshader01.vs", "fragmentshader.fs");
-
     // ---------------------------------------------------------------------------------------------------------
     // TEXTURE
-    unsigned int texture1, texture2;
-    std::string texturePath = PROJECT_SOURCE_DIR "\\Glitter\\Shaders\\Assets\\";
-
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    
-    // load and generate the texture
-    int width, height, nrChannels;
-    unsigned char* data = stbi_load((texturePath + "container.jpg").c_str(), &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
-
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load and generate the texture2
-    stbi_set_flip_vertically_on_load(true);
-    data = stbi_load((texturePath + "awesomeface.png").c_str(), &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
+    unsigned int texture1{}, texture2{};
+    loadTexture(texture1, "container.jpg", false, GL_RGB, GL_RGB);
+    loadTexture(texture2, "awesomeface.png", true, GL_RGBA, GL_RGBA);
 
     // ---------------------------------------------------------------------------------------------------------
+    // BUFFER
     float vertices[] = {
         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
          0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -199,21 +175,18 @@ int main(int argc, char* argv[]) {
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-
     // ---------------------------------------------------------------------------------------------------------
     shader.Use();
+
     glUniform1i(glGetUniformLocation(shader.ID, "texture1"), 0);
     glUniform1i(glGetUniformLocation(shader.ID, "texture2"), 1);
-    
+
     // ---------------------------------------------------------------------------------------------------------
     // Rendering Loop
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     while (glfwWindowShouldClose(mWindow) == false) {
-        
         processInput(mWindow);
-        
-        // -----------------------------------------------------------------------------------------------------
-        // Background Fill Color
+
         glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -233,7 +206,6 @@ int main(int argc, char* argv[]) {
         float currentFrame  =   glfwGetTime();
         deltaTime           =   currentFrame - lastFrame;
         lastFrame           =   currentFrame;
-
         // camera/view transformation
         glm::mat4 view = camera.GetViewMatrix();
         shader.setMat4("view", view);
@@ -248,10 +220,48 @@ int main(int argc, char* argv[]) {
             float angle         =   20.0f * i;
             model               =   glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             glUniformMatrix4fv(glGetUniformLocation(shader.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
-
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
+        
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        // TODO refactor Imgui to seperate functions
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
 
+        // -----------------------------------------------------------------------------------------------------
+        // Background Fill Color
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
         glfwPollEvents();
@@ -259,6 +269,9 @@ int main(int argc, char* argv[]) {
     // optional: de-allocate all resources once they've outlived their purpose:
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     // glfw: terminate, clearing all previously allocated GLFW resources.
     glfwTerminate();
     return EXIT_SUCCESS;
@@ -311,12 +324,42 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     lastX = xpos;
     lastY = ypos;
 
-    camera.ProcessMouseMovement(xoffset, yoffset);
+    if (glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED)
+        camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
 // ---------------------------------------------------------------------------------------------------------
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-    camera.ProcessMouseScroll(yoffset);
+    if (glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED)
+        camera.ProcessMouseScroll(yoffset);
+}
+
+// May need to add more image specific paramenters TODO Make a Texture loading class
+void loadTexture(unsigned int &texture, std::string source, bool isFlipVerticle, GLint internalFormat, GLint format)
+{
+    std::string texturePath = PROJECT_SOURCE_DIR "\\Glitter\\Shaders\\Assets\\";
+    glGenTextures(1, &texture); glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    if (isFlipVerticle)
+        stbi_set_flip_vertically_on_load(true);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load((texturePath + source).c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 }
